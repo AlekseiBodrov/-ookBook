6feddf163ad90c5cac19580c42cc67d45c472d4e
Dmitry <dimapaull@yandex.ru>
2023-03-01 10:58:13 +0300

    Add extensions(UIColor, UIFont, UIView)

---
+++ Extension/UIView + Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIView + Extension.swift
+//  СookBook
+//
+//  Created by Mac OS on 01.03.2023.
+//
+
+import UIKit
+
+extension UIView {
+    func rounded(radius: CGFloat = 12) {
+        self.layer.cornerRadius = radius
+    }
+}
